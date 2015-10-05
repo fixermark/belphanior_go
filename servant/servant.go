7563463091519c01d3ae82a7c3fc9a6257bcb267
Mark Tomczak <fixermark@gmail.com>
2015-10-05 01:51:24 -0400

    Added role reporting at /protocol endpoint.

--- servant/servant.go
+++ servant/servant.go
@@ -1,11 +1,15 @@
 package servant
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"reflect"
 	"regexp"
+	"strconv"
 )
 
 type matchHandler struct {
@@ -28,33 +32,41 @@ func (s *Servant) SetRoleUrl(url string) {
 }
 
 // Adds the handler to the set of registered handlers and serves it
-func (s *Servant) RegisterHandler(description Handler) error {
-	var err error
+func (s *Servant) RegisterHandler(description Handler) {
 	s.role.Handlers = append(s.role.Handlers, description)
 	mh := matchHandler{
 		name: description.Name,
 		method: description.Method,
 		handler: reflect.ValueOf(description.Handler),
 	}
-	mh.pathRegexp, err =
-		substitutionToRegexp(description.Path)
-	if err != nil {
-		return err
-	}
+	mh.pathRegexp = substitutionToRegexp(description.Path)
 	if description.Data != "" {
-		mh.dataRegexp, err =
-			substitutionToRegexp(description.Data)
-		if err != nil {
-			return err
-		}
+		mh.dataRegexp = substitutionToRegexp(description.Data)
 	}
 
 	s.handlers = append(s.handlers, mh)
-	return nil
+}
+
+func (s *Servant) ReportRoles(w http.ResponseWriter, r *http.Request) {
+	var protocol struct {
+		Roles []RoleImplementation `json:"roles"`
+	}
+	protocol.Roles = append(protocol.Roles, s.role)
+
+	encoded, err := json.Marshal(protocol)
+	if err != nil {
+		reportError(w, err)
+		return
+	}
+
+	headers := w.Header()
+	headers.Add("Content-Type", "application/json")
+
+	w.Write(encoded)
 }
 
 // Attempts to call the handler that matches to the given request
-func(s *Servant) CallHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Servant) CallHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	var data string
 	if (r.Body != nil) {
@@ -86,7 +98,6 @@ func(s *Servant) CallHandler(w http.ResponseWriter, r *http.Request) {
 				continue
 			}
 		}
-		fmt.Println("Path matches: ", pathMatches)
 		var pathArgs, dataArgs []string
 		if pathMatches != nil {
 			pathArgs = pathMatches[1:]
@@ -116,7 +127,6 @@ func runHandler(
 	for _, value := range dataMatches {
 		inputValues = append(inputValues, reflect.ValueOf(value))
 	}
-	fmt.Println("Argument count: ", len(inputValues))
 	result := handler.handler.Call(inputValues)
 	switch len(result) {
 	case 0:
@@ -143,21 +153,26 @@ func reportError(w http.ResponseWriter, err error) {
 
 // Convert a Belphanior role substitution to a regexp that will match strings
 // conforming to the role substitution.
-func substitutionToRegexp(sub string) (r *regexp.Regexp, err error) {
-	fmt.Println("substitutionToRegexp: ", sub)
+func substitutionToRegexp(sub string) *regexp.Regexp {
 	argMatcher := regexp.MustCompile("\\$\\(.*?\\)")
 	regexpString := argMatcher.ReplaceAllString(sub, "(.*)")
-	r, err = regexp.Compile(regexpString)
-	fmt.Println("substituted regexp: ", r)
-	return
+	return regexp.MustCompile(regexpString)
 }
 
-// General notes on implementation:
-// - When handlers are registered, dissect them into two regexes (for path and body) and an arg count (for path and body)
-// - Match: method, path and body regex must match method, path and body of request
-// - On match, bundle args into Value slice (path followed by body)
-// - Call handler with args
-// - Marshall handler return value (if any) to string and respond with it and 200
-// - if no matches, 404.
+// Runs the servant as an HTTP server using net/http.  Defines the 'port' flag,
+// which allows specification of the port the server runs on.
 
+func (s *Servant) Run() {
+	portFlag := flag.Int(
+		"port",
+		8080,
+		"The port the servant should listen on.")
+	flag.Parse()
 
+	// TODO(mtomczak): Role reporting.
+	http.HandleFunc("/protocol", s.ReportRoles)
+	http.HandleFunc("/", s.CallHandler)
+
+	fmt.Printf("Servant is listening on port %d\n", *portFlag)
+	http.ListenAndServe(":" + strconv.Itoa(*portFlag), nil)
+}
--- servant/servant_test.go
+++ servant/servant_test.go
@@ -1,16 +1,56 @@
 package servant
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+func TestRoleReport(t *testing.T) {
+	s := Servant{}
+	s.SetRoleUrl("https://example.com/foo")
+	s.RegisterHandler(Handler{
+		Name: "test 1",
+		Method: "GET",
+		Path: "/test1/$(arg 1)",
+		Handler: func() { },
+	})
+	s.RegisterHandler(Handler{
+		Name: "test 2",
+		Method: "POST",
+		Path: "test2/$(arg 1)",
+		Data: "$(arg 2)",
+		Handler: func() { },
+	})
+	req, err := http.NewRequest("GET", "http://example.com/protocol", nil)
+	if err != nil {
+		t.Error("Could not create request: ", err)
+	}
+	resp := httptest.NewRecorder()
+	s.ReportRoles(resp, req)
+	if resp.Code != 200 {
+		t.Error("Expected 200 response, was ", resp.Code)
+	}
+	body := resp.Body.String()
+	const match = "{\"roles\":[{\"role_url\":\"https://example.com/foo\"," +
+		"\"handlers\":[{\"name\":\"test 1\",\"method\":\"GET\"," +
+		"\"path\":\"/test1/$(arg 1)\"},{\"name\":\"test 2\"," +
+		"\"method\":\"POST\",\"path\":\"test2/$(arg 1)\"," +
+		"\"data\":\"$(arg 2)\"}]}]}"
+	if body != match {
+		t.Errorf(
+			"Protocol did not match, needed\n%s\nwas\n%s\n",
+			match, body)
+	}
+}
+
+
 func TestHandlerRegistrationAndCall(t *testing.T) {
 	s := Servant{}
 	handlerInput := "unmodified"
 
-	err := s.RegisterHandler(Handler{
+	s.RegisterHandler(Handler{
 		Name: "test 1",
 		Method: "GET",
 		Path: "/test1/$(arg 1)",
@@ -19,9 +59,6 @@ func TestHandlerRegistrationAndCall(t *testing.T) {
 			return "ok"
 		},
 	})
-	if err != nil {
-		t.Error("Could not register handler: ", err)
-	}
 
 	req, err := http.NewRequest("GET", "http://example.com/test1/argumentReceived", nil)
 	if err != nil {
@@ -46,7 +83,7 @@ func TestNoHandlerForMessage(t *testing.T) {
 	s := Servant{}
 	handlerInput := "unmodified"
 
-	err := s.RegisterHandler(Handler{
+	s.RegisterHandler(Handler{
 		Name: "test 1",
 		Method: "GET",
 		Path: "/test1/$(arg 1)",
@@ -55,11 +92,6 @@ func TestNoHandlerForMessage(t *testing.T) {
 			return "ok"
 		},
 	})
-
-	if err != nil {
-		t.Error("Could not register handler: ", err)
-	}
-
 
 	req, err := http.NewRequest("GET", "http://example.com/notatest", nil)
 	if err != nil {
@@ -73,4 +105,125 @@ func TestNoHandlerForMessage(t *testing.T) {
 	if resp.Code != 404 {
 		t.Error("Expected 404 response, was ", resp.Code)
 	}
+}
+
+func TestMultipleHandlerRegistration(t *testing.T) {
+	s := Servant{}
+	handlerInput := "unmodified"
+
+	s.RegisterHandler(Handler{
+		Name: "test 1",
+		Method: "GET",
+		Path: "/test1/$(arg 1)",
+		Handler: func(arg1 string) string {
+			handlerInput = arg1
+			return "ok"
+		},
+	})
+	s.RegisterHandler(Handler{
+		Name: "test 2",
+		Method: "GET",
+		Path: "/test2/$(arg 1)/$(arg 2)",
+		Handler: func(arg1, arg2  string) string {
+			handlerInput = arg1 + arg2
+			return "ok"
+		},
+	})
+
+	req, err := http.NewRequest("GET", "http://example.com/test2/cat/dog", nil)
+	if err != nil {
+		t.Error("Could not create request: ", err)
+	}
+	resp := httptest.NewRecorder()
+	s.CallHandler(resp, req)
+	if handlerInput != "catdog" {
+		t.Error("Expected arg1 to be catdog, was", handlerInput)
+	}
+	if resp.Code != 200 {
+		t.Error("Expected 200 response, was ", resp.Code)
+	}
+	body := resp.Body.String()
+	if body != "ok" {
+		t.Error("Expected 'ok' body, was ", body)
+	}
+
+}
+
+func TestNoHandlerResult(t *testing.T) {
+	s := Servant{}
+	handlerInput := "unmodified"
+
+	s.RegisterHandler(Handler{
+		Name: "test 1",
+		Method: "GET",
+		Path: "/test1/$(arg 1)",
+		Handler: func(arg1 string) {
+			handlerInput = arg1
+		},
+	})
+
+	req, err := http.NewRequest("GET", "http://example.com/test1/catdog", nil)
+	if err != nil {
+		t.Error("Could not create request: ", err)
+	}
+	resp := httptest.NewRecorder()
+	s.CallHandler(resp, req)
+	if handlerInput != "catdog" {
+		t.Error("Expected arg1 to be catdog, was", handlerInput)
+	}
+	if resp.Code != 200 {
+		t.Error("Expected 200 response, was ", resp.Code)
+	}
+	body := resp.Body.String()
+	if body != "" {
+		t.Error("Expected '' body, was ", body)
+	}
+
+}
+
+func TestErrorHandlerResult(t *testing.T) {
+	s := Servant{}
+
+	s.RegisterHandler(Handler{
+		Name: "test 1",
+		Method: "GET",
+		Path: "/test1/$(arg 1)",
+		Handler: func(arg1 string) (result string, err error) {
+			if arg1 == "bomb" {
+				err = errors.New("kaboom")
+			} else {
+				result = arg1
+			}
+			return
+		},
+	})
+
+	req, err := http.NewRequest("GET", "http://example.com/test1/catdog", nil)
+	if err != nil {
+		t.Error("Could not create request: ", err)
+	}
+	resp := httptest.NewRecorder()
+	s.CallHandler(resp, req)
+	if resp.Code != 200 {
+		t.Error("Expected 200 response, was ", resp.Code)
+	}
+	body := resp.Body.String()
+	if body != "catdog" {
+		t.Error("Expected 'catdog' body, was ", body)
+	}
+
+	req, err = http.NewRequest("GET", "http://example.com/test1/bomb", nil)
+	if err != nil {
+		t.Error("Could not create request: ", err)
+	}
+	resp = httptest.NewRecorder()
+	s.CallHandler(resp, req)
+	if resp.Code != 500 {
+		t.Error("Expected 500 response, was ", resp.Code)
+	}
+	body = resp.Body.String()
+	if body != "kaboom\n" {
+		t.Error("Expected 'kaboom' body, was", body)
+	}
+
 }
